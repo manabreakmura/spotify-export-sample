662bed9b0481c8d457baaac3c02077e2a8b378ca
manabreakmura <174273321+manabreakmura@users.noreply.github.com>
2025-04-11 17:40:51 +0300

    Add auth

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
+import httpx
 from decouple import config
+
+try:
+    response = httpx.post(
+        "https://accounts.spotify.com/api/token",
+        data={
+            "grant_type": "client_credentials",
+            "client_id": config("SPOTIFY_CLIENT_ID"),
+            "client_secret": config("SPOTIFY_CLIENT_SECRET"),
+        },
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+    )
+    response.raise_for_status()
+    access_token = response.json()["access_token"]
+except Exception as exception:
+    raise exception
