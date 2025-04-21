1f6a18e201d528fa5ada1373a370d6ad26de4023
manabreakmura <174273321+manabreakmura@users.noreply.github.com>
2025-04-21 23:54:34 +0300

    Add playlist metadata to file

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+import json
+import sys
+from datetime import datetime
+
 import httpx
 from decouple import config
 
@@ -15,3 +19,29 @@ try:
     access_token = response.json()["access_token"]
 except Exception as exception:
     raise exception
+
+
+url = f"https://api.spotify.com/v1/playlists/{sys.argv[1]}/tracks"
+results = []
+
+while url:
+    try:
+        response = httpx.get(url, headers={"Authorization": f"Bearer {access_token}"})
+        response.raise_for_status()
+        response = response.json()
+        url = response["next"]
+
+        for obj in response["items"]:
+            results.append(
+                {
+                    "artists": ", ".join([x["name"] for x in obj["track"]["artists"]]),
+                    "name": obj["track"]["name"],
+                    "album": obj["track"]["album"]["name"],
+                }
+            )
+    except Exception as exception:
+        raise exception
+
+timestamp = datetime.now().isoformat()
+with open(f"{timestamp}.json", "w") as file:
+    json.dump(results, file, indent=4, ensure_ascii=False)
